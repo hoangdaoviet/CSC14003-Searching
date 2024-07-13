86d65dc8c388774e9f21cac7929e061f84e11564
Dao Viet Hoang <22127121@student.hcmus.edu.vn>
2024-07-13 21:18:27 +0700

    Small changes

--- helper_functions.py
+++ helper_functions.py
@@ -5,7 +5,7 @@ def exportToFile(algoName, path, time, memory, output):
         for i in range(len(path)):
             f.write(str(path[i]))
             if i != len(path) - 1:
-                f.write(' ->  ')
+                f.write(' -> ')
         f.write('\n')
         f.write(f'Time in seconds: {time}\n')
         f.write(f'Memory in KB: {memory / 1024}\n\n')
